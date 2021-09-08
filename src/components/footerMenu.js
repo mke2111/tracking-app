54a4b179ec2369c4250f8f19d1885f3401501e6a
mke2111 <roymke134@gmail.com>
2021-09-08 07:43:35 +0300

    Add footer styles

--- src/components/footerMenu.js
+++ src/components/footerMenu.js
@@ -4,20 +4,23 @@ import { withCookies, Cookies } from 'react-cookie';
 const currentLocation = window.location.pathname.split('/')[1];
 
 const FooterMenu = ({ cookies }) => (
-  <footer className="text-3xl">
-    <a href="/" className={currentLocation === '' ? 'blue' : 'gray'} aria-label="games">Home</a>
-    <a href="/Profile" className={currentLocation === 'Profile' ? 'blue' : 'gray'} aria-label="profile">Profile</a>
-    <button
-      className="bg-red-700"
-      id="logout-btn"
-      type="submit"
-      onClick={() => {
-        cookies.remove('uid', { path: '/' });
-        window.location.reload();
-      }}
-    >
-      Log out
-    </button>
+  <footer className="text-3xl fixed bottom-0 flex flex-row w-full justify-around">
+    <div className="bg-blue-400 pb-1 flex flex-row w-full justify-around">
+      <a href="/" className={currentLocation === '' ? 'blue' : 'gray'} aria-label="games">Home</a>
+      <a href="/Profile" className={currentLocation === 'Profile' ? 'blue' : 'gray'} aria-label="profile">Profile</a>
+    </div>
+    <div className="bg-red-700 w-36 px-1">
+      <button
+        id="logout-btn"
+        type="submit"
+        onClick={() => {
+          cookies.remove('uid', { path: '/' });
+          window.location.reload();
+        }}
+      >
+        Log out
+      </button>
+    </div>
   </footer>
 );
 
